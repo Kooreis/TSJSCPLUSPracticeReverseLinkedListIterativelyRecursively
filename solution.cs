0f85be25778dd636d46d6178937605a0b3717f83
Kooreis <kooreis.official@gmail.com>
2022-10-16 21:17:34 -0500

    This part of the script defines a function 'ListNode' that creates a linked list node. It takes two parameters: 'val' and 'next'. If 'val' is undefined, it will be set to 0; if 'next' is undefined, it will be set to null.

--- solution.cs
+++ solution.cs
@@ -1,40 +1,4 @@
-Here is a JavaScript solution for reversing a linked list both iteratively and recursively:
-
-```javascript
 function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
 }
-
-// Iterative solution
-function reverseListIteratively(head) {
-    let prev = null;
-    let current = head;
-    while(current != null) {
-        let nextTemp = current.next;
-        current.next = prev;
-        prev = current;
-        current = nextTemp;
-    }
-    return prev;
-}
-
-// Recursive solution
-function reverseListRecursively(head) {
-    if(head === null || head.next === null) return head;
-    let p = reverseListRecursively(head.next);
-    head.next.next = head;
-    head.next = null;
-    return p;
-}
-
-// Test
-let node3 = new ListNode(3, null);
-let node2 = new ListNode(2, node3);
-let node1 = new ListNode(1, node2);
-
-console.log(reverseListIteratively(node1));
-console.log(reverseListRecursively(node1));
-```
-
-This code first defines a `ListNode` function to create a linked list node. Then it defines two functions `reverseListIteratively` and `reverseListRecursively` to reverse a linked list iteratively and recursively, respectively. Finally, it creates a linked list with three nodes and tests the two functions.
