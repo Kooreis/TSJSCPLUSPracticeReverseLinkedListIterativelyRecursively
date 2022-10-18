9443ece254494c7df739d4cae5e09eafd4135882
Kooreis <kooreis.official@gmail.com>
2022-10-17 21:17:34 -0500

    This part of the script defines a function 'reverseListIteratively' that reverses a linked list iteratively. It takes the head of the linked list as a parameter and returns the head of the reversed linked list.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,11 @@
-function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+function reverseListIteratively(head) {
+    let prev = null;
+    let current = head;
+    while(current != null) {
+        let nextTemp = current.next;
+        current.next = prev;
+        prev = current;
+        current = nextTemp;
+    }
+    return prev;
 }
